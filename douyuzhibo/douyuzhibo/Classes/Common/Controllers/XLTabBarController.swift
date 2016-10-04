62186ba3a70c1344ad922c6f3ff47e43e246e843
xianglin <xianglin@domob.cn>
2016-10-04 20:46:13 +0800

    初始化项目02

---
+++ douyuzhibo/douyuzhibo/Classes/Common/Controllers/XLTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  XLTabBarController.swift
+//  douyuzhibo
+//
+//  Created by xianglin on 16/10/4.
+//  Copyright © 2016年 xianglin. All rights reserved.
+//
+
+import UIKit
+
+class XLTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
